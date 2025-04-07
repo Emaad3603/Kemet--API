5d337bd38a71a3552abaa0c23c69081e1db48226
Osamayasser20 <125865637+Osamayasser20@users.noreply.github.com>
2025-04-08 01:03:54 +0200

    admin  commit

---
+++ Kemet.APIs/DTOs/HomePageDTOs/AddPlaceDtos.cs
@@ -0,0 +1,28 @@
+﻿using Kemet.Core.Entities;
+
+namespace Kemet.APIs.DTOs.HomePageDTOs
+{
+    public class AddPlaceDtos
+    {
+        public int PlaceID { get; set; }
+    
+      //  public double AverageRating { get; set; }
+       // public int? RatingsCount { get; set; }
+
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string CategoryName { get; set; }
+        public string CultureTips { get; set; }
+        public string Duration { get; set; }
+      //  public int? priceId { get; set; }
+        public decimal EgyptianAdultCost { get; set; }
+        public decimal EgyptianStudentCost { get; set; }
+        public decimal TouristAdultCost { get; set; }
+        public decimal TouristStudentCost { get; set; }
+        public List<string> ImageURLs { get; set; }
+
+
+      
+
+    }
+}
